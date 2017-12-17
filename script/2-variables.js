0672c41162926fd01aff257ce23f0729aa62b740
unknown <urszula giewon@gmail.com>
2017-12-17 09:53:03 +0100

    Dodanie pierwszych zmiennych

---
+++ script/2-variables.js
@@ -0,0 +1,15 @@
+'use strict';
+// console.log("Urzszula");
+
+var imie;
+var imieDamskie = "Urszula";
+var wiek = 29;
+var kolor = "Różowy";
+
+imie = "Sergio";
+imie = imieDamskie;
+
+//console.log(imie);
+console.log(wiek);
+console.log(kolor);
+
