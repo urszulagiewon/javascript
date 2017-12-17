6d2be5a6377c035adf5e8b88ada10015e874c500
unknown <urszula giewon@gmail.com>
2017-12-17 11:01:26 +0100

    funkcje

--- script/3-function.js
+++ script/3-function.js
@@ -1,1 +1,20 @@
-'use strict'
+'use strict';
+function obliczOdsetki(kapital, procent) {
+    var kapitalZOdsetkami = kapital + kapital * procent;
+    
+    return kapitalZOdsetkami;
+    
+}
+
+// document.write(obliczOdsetki(10000000,0.015)); 
+
+var inwestycjaAnety = obliczOdsetki(10000000,0.015);
+
+var inwestycjaKuby = obliczOdsetki(1000,0.05);
+
+console.log(inwestycjaKuby);
+
+console.log(inwestycjaAnety); 
+
+
+
