43a1f0dfb7d37ae9100b5de1765db841f1daadc8
unknown <urszula giewon@gmail.com>
2017-12-17 12:50:27 +0100

    Tablice

---
+++ script/6-arrays.js
@@ -0,0 +1,30 @@
+'use strict';
+var imiona = ["Urszula","Karolia","Gosia","Marta","Ewa"];
+
+/* 
+imiona[2] = "Anna";
+
+imiona.push("Andrzej");
+
+imiona.unshift("Hubert");
+
+console.log(imiona.unshift("Hubert"));
+console.log(imiona); 
+*/
+
+// console.log(imiona.shift());
+
+
+//console.log(imiona);
+//console.log(imiona.length);
+
+/* var stringZTablicy = imiona.join(" \+-+/ ");
+console.log(stringZTablicy); */
+
+console.log(imiona.reverse());
+
+var tablicaLiczb = [3, 89, 4, 333, 24, 90, 442];
+
+var posortowanaTablica = imiona.sort();
+
+console.log(posortowanaTablica);
