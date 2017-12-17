0678d0470ae32e637e9a0f10667ea352d46bbd83
unknown <urszula giewon@gmail.com>
2017-12-17 14:55:27 +0100

    operatory

---
+++ script/9-operatory.js
@@ -0,0 +1,30 @@
+'use strict';
+/* var wynik;
+wynik = 19 % 5 * 4;
+console.log(wynik); */
+
+
+var liczba1=13; var liczba2=5;
+var wynik;
+
+liczba1 =- liczba2;
+
+console.log(liczba1);
+
+if (2=="2") {
+    console.log("prawda");
+}
+else {
+    console.log("fałsz");
+}
+
+/* if (! ("3" == 2) && (7>1)) {
+    document.write("Prawda");   
+}
+else {
+    document.write("Fałsz");
+} */
+
+if (false || true) {
+    document.write("Taaa, jasne");
+}
