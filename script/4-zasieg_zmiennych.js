4da53d68192474b231663f376996ab244c895a1a
unknown <urszula giewon@gmail.com>
2017-12-17 11:34:31 +0100

    funkcja policzSumaKasy

---
+++ script/4-zasieg_zmiennych.js
@@ -0,0 +1,20 @@
+var ilosc1 = 3500;
+var ilosc2 = 3000;
+var ilosc3 = 7500;
+var ilosc4 = 2500;
+
+ilosc2 = 3000;
+
+function policzSumeKasy(iloscOsoby1, iloscOsoby2, iloscOsoby3, iloscOsoby4, wiek) {
+    'use strict';
+    var sumaKasy;
+    sumaKasy = iloscOsoby1 + iloscOsoby2 + iloscOsoby3 + iloscOsoby4;
+    
+    var osoba = [sumaKasy, wiek];
+    
+    return osoba;
+}
+
+var wynikObliczen = policzSumeKasy(ilosc1, ilos2, ilosc3, ilosc4, 25);
+
+console.log();
