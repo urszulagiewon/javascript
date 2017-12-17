82eb113e71583e3e147fcd323a6043f01b0f377c
unknown <urszula giewon@gmail.com>
2017-12-17 12:17:08 +0100

    zmienne proste

---
+++ script/5-typy_danych.js
@@ -0,0 +1,30 @@
+'use strict';
+/* var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia;
+
+console.log(calkowitaWyplata); */
+
+// łańcuch znaków string 
+
+var wyplata = "5000";
+var premia ="2500";
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia;
+
+var zdanie = "Ala \" ma kota";
+
+console.log(zdanie);
+
+var czyJestSmog;
+czyJestSmog = false;
+
+if (czyJestSmog) {
+    console.log("Weź ze sobą maseczkę");
+}
+    else {
+        console.log("Ale fajnie!");
+    }
